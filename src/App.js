60c6c51c328c2421971081e80a7f97f36fb0fd0a
Pratik-0100 <2018btt028@sggs.ac.in>
2022-06-28 11:13:55 +0530

    react project

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+
+function App() {
+ const [mode, setMode] = useState("light")
+
+ const [alert, setAlert] = useState(null)
+
+ const showAlert = (message,type)=>{
+  setAlert({
+    msg:message,
+    type:type
+  })
+  setTimeout(() => {
+    setAlert(null);
+    
+  }, 1000);
+
+ }
+
+
+ const toggleMode =()=>{
+  if(mode==="light"){
+    setMode("dark")
+    document.body.style.backgroundColor = "#113a59";
+    showAlert("Dark mode has been enabled","success");
+  }
+  else{
+    setMode("light")
+    document.body.style.backgroundColor = "white";
+    showAlert("Light mode has been enabled","success");
+
+  }
+  
+  
+
+ }
+  return (
+    <>
+    <Router>
+    <Navbar mode={mode} toggleMode={toggleMode}/>
+    <Alert aletr={alert}/>
+    <div className="container my-3">
+    <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+         
+          <Route path="/home">
+          <TextForm showAlert={showAlert}heading="Enter Your Text Below" mode={mode}/>
+          </Route>
+        </Switch>
+      
+     
+    </div>
+   
+    </Router>
+    </>
+            
+        
+      
+      
+  );
+}
+
+export default App;
